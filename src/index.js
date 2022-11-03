1a18cac92a0ed83f1f891bab10823d28466e14f2
Miikeo <Miguel.A.INCO@gmail.com>
2022-11-02 21:57:15 -0600

    First commit, using a template, calling and using an avocados' API, working with the data from API request, and finally creating HTML elements in the DOM

--- src/index.js
+++ src/index.js
@@ -3,4 +3,24 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = "https://platzi-avo.vercel.app/api/avo";
+
+//Web api fetch
+window.fetch(url)
+    .then(answer => answer.json())
+    .then(answerJSON => {
+        const allItems = [];
+        answerJSON.data.forEach(item => {
+            //Create image
+            const image = document.createElement('img');
+            //Create tittle
+            const tittle = document.createElement('h2');
+            //Create price
+            const price = document.createElement('div');
+
+            const container = document.createElement('div');
+            container.append(image, tittle, price);
+            allItems.push(container);
+        });
+        document.body.append(...allItems);
+    })
