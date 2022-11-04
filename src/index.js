67e8ddfdd8b84f50572bac855f906ed63b012fc2
Miikeo <Miguel.A.INCO@gmail.com>
2022-11-03 18:21:47 -0600

    classes were added to elements created in the DOM, and values were added in the corresponding elements

--- src/index.js
+++ src/index.js
@@ -3,24 +3,39 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-const url = "https://platzi-avo.vercel.app/api/avo";
+const baseURL = "https://platzi-avo.vercel.app";
 
+const appNode = document.querySelector('#app');
+
+const formatPrice = (price) => {
+
+     const newPrice = new window.Intl.NumberFormat('en-En',{
+        style: 'currency',
+        currency: 'USD',
+    }).format(price);
+    return newPrice;
+}
 //Web api fetch
-window.fetch(url)
+window.fetch(`${baseURL}/api/avo`)
     .then(answer => answer.json())
     .then(answerJSON => {
         const allItems = [];
         answerJSON.data.forEach(item => {
             //Create image
             const image = document.createElement('img');
+            image.src = `${baseURL}${item.image}`;
             //Create tittle
             const tittle = document.createElement('h2');
+            tittle.className = 'text-lg';
+            tittle.textContent = item.name;
+            
             //Create price
             const price = document.createElement('div');
+            price.textContent = formatPrice(item.price);
 
             const container = document.createElement('div');
             container.append(image, tittle, price);
             allItems.push(container);
         });
-        document.body.append(...allItems);
+        appNode.append(...allItems);
     })
